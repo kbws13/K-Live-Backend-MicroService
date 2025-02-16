f46639b9d5c0812d1f63f746fc51229e05d7c3a2
kbws13 <hsy040506@163.com>
2025-02-16 21:53:40 +0800

    补充提交

--- sql/db.sql
+++ sql/db.sql
@@ -238,3 +238,18 @@ create table statisticInfo
     dataType      tinyint(1)  not null comment '数据统计类型',
     count         int default null comment '统计数量'
 ) comment '数据统计表';
+
+CREATE TABLE `undo_log`
+(
+    `branch_id`     bigint       NOT NULL COMMENT 'branch transaction id',
+    `xid`           varchar(128) NOT NULL COMMENT 'global transaction id',
+    `context`       varchar(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+    `rollback_info` longblob     NOT NULL COMMENT 'rollback info',
+    `log_status`    int          NOT NULL COMMENT '0:normal status,1:defense status',
+    `log_created`   datetime(6)  NOT NULL COMMENT 'create datetime',
+    `log_modified`  datetime(6)  NOT NULL COMMENT 'modify datetime',
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`),
+    KEY `ix_log_created` (`log_created`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci COMMENT ='AT transaction mode undo table';
