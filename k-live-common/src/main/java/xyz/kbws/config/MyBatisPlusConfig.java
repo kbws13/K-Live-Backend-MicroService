3ba7119504fcd828fad996810c1dcc73260df2bd
kbws13 <hsy040506@163.com>
2025-04-16 23:29:37 +0800

    feat(k-live-common): 添加 MyBatis Plus 分页拦截器配置 - 新增 MyBatisPlusConfig 类，配置分页插件 - 使用 MybatisPlusInterceptor拦截器 - 集成 PaginationInnerInterceptor 分页插件，支持 MySQL 数据库

---
+++ k-live-common/src/main/java/xyz/kbws/config/MyBatisPlusConfig.java
@@ -0,0 +1,26 @@
+package xyz.kbws.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author kbws
+ * @date 2025/4/16
+ * @description:
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    /**
+     * 拦截器配置
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
